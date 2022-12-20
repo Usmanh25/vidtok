cc0ee97d9f969a1f311cd6e83087694ae7af3efc
usmanh25 <usmanh25@yahoo.com>
2022-12-20 03:53:22 -0800

    env

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   images: {
     domains: [
       'p16-sign-va.tiktokcdn.com', 
-      'thenounproject.com', 
       'lh3.googleusercontent.com', 
       'p16-sign-sg.tiktokcdn.com',
       'p77-sign-va.tiktokcdn.com',
