84784c52170f9e7a7c7af74c78a5b6a00b029a3e
usmanh25 <usmanh25@yahoo.com>
2022-12-20 05:23:14 -0800

    ready for deployment

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -38,14 +38,14 @@ const VideoCard: NextPage<IProps> = ({post}) => {
     return (
         <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
           <div>
-            <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded '>
+            <div className='flex gap-3 p-2 font-semibold rounded '>
               <div className='md:w-16 md:h-16 w-10 h-10'>
                 <Link href={`/profile/${post.postedBy._id}`}>
                   <>
                     <Image
                       width={62}
                       height={62}
-                      className=' rounded-full'
+                      className='cursor-pointer rounded-full'
                       src={post.postedBy.image}
                       alt='profile photo'
                       layout='responsive'
@@ -55,13 +55,10 @@ const VideoCard: NextPage<IProps> = ({post}) => {
               </div>
               <div>
                 <Link href={`/profile/${post.postedBy._id}`}>
-                  <div className='flex items-center gap-2'>
+                  <div className='flex items-center gap-2 cursor-pointer'>
                     <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
                       {post.postedBy.userName}{' '}
                       <GoVerified className='text-blue-400 text-md' />
-                    </p>
-                    <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
-                      {post.postedBy.userName}
                     </p>
                   </div>
                 </Link>
@@ -73,7 +70,7 @@ const VideoCard: NextPage<IProps> = ({post}) => {
             <div
               onMouseEnter={() => setIsHover(true)}
               onMouseLeave={() => setIsHover(false)}
-              className='rounded-3xl'
+              className='rounded-3xl bg-gray-200'
             >
               <Link href={`/detail/${post._id}`}>
                 <video
