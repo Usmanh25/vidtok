f5ef0c3e13fd7b2c3e8b15d2a2f79d8217aafcf2
usmanh25 <usmanh25@yahoo.com>
2023-02-24 02:19:08 -0800

    images

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
       'p16-sign-va.tiktokcdn.com', 
       'lh3.googleusercontent.com', 
       'p16-sign-sg.tiktokcdn.com',
-      'www.shutterstock.com',
       'p77-sign-va.tiktokcdn.com',
       'p77-sign-va-lite.tiktokcdn.com'
     ],
